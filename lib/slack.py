c74722bdac2a9eee9ec08301e624e7c70f6ae3c2
akira akagawa <akariver@gmail.com>
2023-09-20 21:14:18 +0900

    微修正

--- lib/slack.py
+++ lib/slack.py
@@ -106,7 +106,7 @@ class SlackClient:
         messages_text = []
         for message in messages[::-1]:
             # Ignore bot messages and empty messages and U03F67RAW以外の user_id
-            if "bot_id" in message or len(message["text"].strip()) == 0 or "U03F67RAW" in message:
+            if "bot_id" in message or len(message["text"].strip()) == 0 or message["user"] != "U03F67RAW"
             continue
 
             # Get speaker name
